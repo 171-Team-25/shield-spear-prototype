87e198d59a06a93dddca42665237da8baf8334b3
JackWilhelm <jgwilhel@ucsc.edu>
2025-01-24 12:23:30 -0800

    Charging preview

--- Assets/KnockbackPunch.cs
+++ Assets/KnockbackPunch.cs
@@ -22,7 +22,7 @@ public class KnockbackPunch : MonoBehaviour
 
     private PlayerInput _playerInput;
     private float charge = 0;
-    private float chargeRate = 1f;
+    private float chargeRate = 1.2f;
     private string[] TagsOfHittables = {"Offense", "Defense", "Enemy"};
     // Start is called before the first frame update
     void Start()
@@ -37,18 +37,20 @@ public class KnockbackPunch : MonoBehaviour
     void Update()
     {
         if (knockbackCooldown <= 0 && _playerInput.actions["Ability4"].ReadValue<float>() > 0) {
-            charge += chargeRate * Time.deltaTime;
+            charge = Mathf.Min(4, charge + (chargeRate * Time.deltaTime));
             Debug.Log("charge" + charge);
+            visualizer.transform.localScale = new Vector3(1, (maxHeight + charge)/2, 1);
         }
-        if (_playerInput.actions["Ability4"].ReadValue<float>() <= 0 && charge > 0) {
+        if (!isKnockbacking && _playerInput.actions["Ability4"].ReadValue<float>() <= 0 && charge > 0) {
            isKnockbacking = true;
            knockbackCooldown = knockbackRate;
            knockbackTimer = 0;
+           Debug.Log("charge release");
         }
         if (isKnockbacking) {
             knockbackTimer += Time.deltaTime;
-            newHeight = Mathf.Lerp(0, maxHeight, Mathf.Clamp01(knockbackTimer/durationTillFull));
-            visualizerLength = Mathf.Lerp(0.5f, maxHeight/2, Mathf.Clamp01(knockbackTimer/durationTillFull));
+            newHeight = Mathf.Lerp(0, maxHeight + charge, Mathf.Clamp01(knockbackTimer/durationTillFull));
+            visualizerLength = Mathf.Lerp(0.5f, (maxHeight + charge)/2, Mathf.Clamp01(knockbackTimer/durationTillFull));
             if (knockbackTimer >= durationTillFull) {
                 isKnockbacking = false;
                 charge = 0;
@@ -58,12 +60,13 @@ public class KnockbackPunch : MonoBehaviour
                     hitEntities.Clear();
                 }
             }
+            knockbackCollider.height = newHeight;
+        transform.localPosition = new Vector3(0, 0, newHeight);
+        visualizer.transform.localScale = new Vector3(1, visualizerLength, 1);
         }
         if (!isKnockbacking && charge <= 0) {
             knockbackCooldown -= Time.deltaTime;
         }
-        knockbackCollider.height = newHeight;
-        transform.localPosition = new Vector3(0, 0, newHeight);
-        visualizer.transform.localScale = new Vector3(1, visualizerLength, 1);
+        
     }
 }
