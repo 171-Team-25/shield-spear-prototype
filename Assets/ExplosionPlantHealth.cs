331e668300e845fa55c8939b2c984652f748c093
JackWilhelm <jgwilhel@ucsc.edu>
2025-02-04 14:16:38 -0800

    Explosion disables movement

--- Assets/ExplosionPlantHealth.cs
+++ Assets/ExplosionPlantHealth.cs
@@ -6,6 +6,17 @@ using UnityEngine;
 public class ExplosionPlantHealth : Health
 {
     private Collider plantCollider;
+    private bool isExploding = false;
+    private List<GameObject> hitEntities = new List<GameObject>();
+
+    public float arcHeight = 2f;
+    public float duration = 1f;
+
+    protected string[] TagsOfExplosionHittables =
+    {
+        "Offense",
+        "Defense"
+    };
     void Start()
     {
         plantCollider = GetComponent<Collider>();
@@ -41,12 +52,15 @@ public class ExplosionPlantHealth : Health
 
     void Explode() {
         transform.localScale = new UnityEngine.Vector3(10, 0.5f, 10);
+        isExploding = true;
     }
 
     void Disappear() {
         if (matRenderer != null) {
             matRenderer.enabled = false;
         }
+        isExploding = false;
+        hitEntities.Clear();
     }
 
     void Shrink() {
@@ -57,5 +71,45 @@ public class ExplosionPlantHealth : Health
         isDead = false;
         matRenderer.enabled = true;
         currentHealth = baseHealth;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (isExploding) {
+            for (int i = 0; i < TagsOfExplosionHittables.Length; i++)
+            {
+                if (other.CompareTag(TagsOfExplosionHittables[i]))
+                {
+                    if (!hitEntities.Contains(other.gameObject)) {
+                        hitEntities.Add(other.gameObject);
+                        StartCoroutine(BlastAway(other.gameObject));
+                    }
+                }
+            }
+            
+        }
+    }
+
+    private IEnumerator BlastAway(GameObject blasted) {
+        if (blasted.TryGetComponent<Movement>(out var movement))
+        {
+            blasted.GetComponent<Rigidbody>().velocity = UnityEngine.Vector3.zero;
+            movement.enabled = false;
+        }
+        else if (blasted.TryGetComponent<DefenseMovement>(out var defenseMovement))
+        {
+            blasted.GetComponent<Rigidbody>().velocity = UnityEngine.Vector3.zero;
+            defenseMovement.enabled = false;
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        if (blasted.TryGetComponent<Movement>(out var movement2))
+        {
+            movement2.enabled = true;
+        }
+        else if (blasted.TryGetComponent<DefenseMovement>(out var defenseMovement2))
+        {
+            defenseMovement2.enabled = true;
+        }
     }
 }
